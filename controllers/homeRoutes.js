037bed75f16aa6d98221efed2355e4e4249402f9
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-28 13:09:11 -0400

    psuedo-coded out some of the routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { Character, Chosen, User } = require('../models');
+const withAuth = require('.../utils/auth');
+
+//router get at homepage for and render homepage
+
+//router to login/sign up
+
+//router to profile/character collection
