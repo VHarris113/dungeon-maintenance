2dbc92838688b32d79a1658a203b97a8da1fa01a
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-28 14:32:10 -0400

    adding routes/psuedocoding/more server stuff

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,35 @@ const { Character, Chosen, User } = require('../models');
 const withAuth = require('.../utils/auth');
 
 //router get at homepage for and render homepage
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage', {
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage', {
+            logged_out: req.session.logged_out
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/my-characters', async (req, res) => {
+    try {
+        res.render('my-characters', {
+            characters: req.session.characters
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 //router to login/sign up
 
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-const hbs = exphbs.create({ helpers });
+const models = require('./models');
+// const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
