1ba3cd791cad886e070eb753d1d9a5672e184f3a
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-04-28 11:12:14 -0400

    created Chosen.js model

--- models/Chosen.js
+++ models/Chosen.js
@@ -1,2 +1,24 @@
-//race column
-//class 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Chosen extends Model {}
+
+Chosen.init (
+    {
+        race: {
+            type: DataTypes.STRING
+        },
+        class: {
+            type: DataTypes.STRING 
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'chosen',
+      }
+);
+
+module.exports = Chosen;
