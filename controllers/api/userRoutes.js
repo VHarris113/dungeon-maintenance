d447362ef3b769ff5ca11069b24c6a3cd044973c
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-04-29 11:54:29 -0400

    started working on userRoutes.js with veronica

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// Router post to create sign up
-//login
-//logout
-//homepage
-//character-selection.handlebars
+//if click on home, route to home
+//if logged in, route to character-creation.handlebars
+//post request to create new character
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+        res.redirect('/homepage');
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+  module.exports = router;
+  
