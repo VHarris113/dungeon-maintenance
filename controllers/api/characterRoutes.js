ed6480efbbf5fefbb11b8e5a58b4246af6c56bac
Jules Scheil <juliana.m.scheil@gmail.com>
2021-05-03 09:45:54 -0400

    changes

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -1,75 +1,59 @@
-const router = require('express').Router();
-const { Character, User } = require('../../models');
-const withAuth = require('../../utils/auth');
-const multer = require('multer');
-
+const router = require("express").Router();
+const { Character, User } = require("../../models");
+const withAuth = require("../../utils/auth");
+const multer = require("multer");
+​
 //create a character
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, __dirname + '../../../public/images/');
-    },
-    filename: function (req, file, cb) {
-      cb(null, path.extname(file.originalname));
-    },
-  });
-
-  const upload = multer({ storage: storage });
-
-router.post('/', withAuth, upload.single('image'),async (req, res) => {
-    console.log("##########", req.body);
-    console.log(req.file.originalname)
-    try {
-        const characters = Character.create({
-            ...req.body,
-            image: req.file && req.file.originalname,
-            user_id: req.session.user_id,
-        }); 
-      
-
-        res.render('character-selection', { characters })
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
-
-
+// router.post("/", async (req, res) => {
+//   console.log("##########", req.body);
+//   try {
+//     const newCharacter = Character.create({
+//       ...req.body,
+//       user_id: req.session.user_id,
+//     });
+​
+//     res.render("character-selection", { newCharacter });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+​
 //delete a character
-router.delete('/:character_id', async (req, res) => {
-    // console.log(req.params.character_id)
-    try {
-        const characterData = await Character.destroy({
-            where: {
-                character_id: req.params.character_id,
-                // user_id: req.session.user_id,
-            },
-        });
-
-        if (!characterData) {
-            res.status(404).json({ message: 'No character found with this id!' });
-            return;
-        }
-
-        res.status(200).json(characterData);
-    } catch (err) {
-        res.status(500).json(err);
+router.delete("/:character_id", async (req, res) => {
+  // console.log(req.params.character_id)
+  try {
+    const characterData = await Character.destroy({
+      where: {
+        character_id: req.params.character_id,
+        // user_id: req.session.user_id,
+      },
+    });
+​
+    if (!characterData) {
+      res.status(404).json({ message: "No character found with this id!" });
+      return;
     }
+​
+    res.status(200).json(characterData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-
-// //find all characters by user id 
-router.get('/user/:user_id', async (req, res) => {
-    try {
-        const charData = await Character.findAll({
-            where: {
-                user_id: req.params.user_id
-            }
-        });
-        const characters = charData.map(char => char.get({ plain: true }));
-        // console.log(characters);
-        res.render('character-selection', { characters });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+​
+// //find all characters by user id
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const charData = await Character.findAll({
+      where: {
+        user_id: req.params.user_id,
+      },
+    });
+    const characters = charData.map((char) => char.get({ plain: true }));
+    // console.log(characters);
+    res.render("character-selection", { characters });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 // console.log(req.params.user_id)
 //     try {
@@ -80,53 +64,52 @@ router.get('/user/:user_id', async (req, res) => {
 //         user_id: req.params.user_id
 //     }
 // })
-
+​
 //         const userCharacter = characterData.get({ plain: true });
 //         console.log(userCharacter);
 //         // res.render('character-selection', {
 //         //     ...userCharacter
 //         // });
 //         res(200).json(userCharacter)
-
+​
 //     } catch (err) {
 //         res.status(404);
 //        // res.send("You have no characters to display!");
 //          //  res.redirect('/create');
 //     }
 // });
-
+​
 // //find all characters in db regardless of user
-router.get('/', async (req, res) => {
-    console.log("######### route hit")
-    try {
-        const charData = await Character.findAll({});
-        const characters = charData.map(char => char.get({ plain: true }));
-        // console.log(characters);
-        res.render('character-selection', { characters });
-    } catch (err) {
-        res.status(400).json(err);
-    }
-
+router.get("/", async (req, res) => {
+  console.log("######### route hit");
+  try {
+    const charData = await Character.findAll({});
+    const characters = charData.map((char) => char.get({ plain: true }));
+    // console.log(characters);
+    res.render("character-selection", { characters });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
-
+​
 // //find one character by character id
-
-router.get('/:character_id', async (req, res) => {
-    // console.log(req.params.Character)
-    try {
-        const characterData = await Character.findByPk(req.params.character_id);
-        if (!characterData) {
-            res.status(404).json({ message: 'No character with this id!' });
-            return;
-        }
-        const character = characterData.get({ plain: true });
-        res.render('character-selection', character);
-        // res.status(200).json(character)
-    } catch (err) {
-        res.status(500).json(err);
-    };
+​
+router.get("/:character_id", async (req, res) => {
+  // console.log(req.params.Character)
+  try {
+    const characterData = await Character.findByPk(req.params.character_id);
+    if (!characterData) {
+      res.status(404).json({ message: "No character with this id!" });
+      return;
+    }
+    const character = characterData.get({ plain: true });
+    res.render("character-selection", character);
+    // res.status(200).json(character)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
+​
 // //update a character TODO: we need to decide if we are going to include this or not. We would need a separate page I think.
 // router.put('/:id', async (req, res) => {
 //     try {
@@ -144,7 +127,7 @@ router.get('/:character_id', async (req, res) => {
 //         res.status(500).json(err);
 //     }
 // });
-
+​
 // route for upload picture --- in server file now
 // router.post('/', (req, res) => {
 //     let sampleFile;
@@ -156,45 +139,53 @@ router.get('/:character_id', async (req, res) => {
 //     }
 //     //name of input is sampleFile
 //     sampleFile = req.files.cat;
-
+​
 //     uploadPath = __dirname + '/upload/' + sampleFile.name
 //     console.log(sampleFile);
-
+​
 //     //use mv to put file on server
-
+​
 //     sampleFile.mv(uploadPath, function (err) {
 //         if (err) {
 //             return res.status(500).send(err);
 //         }
-
+​
 //         res.send('File uploaded to ' + uploadPath);
-
+​
 //     });
-
-
+​
 // });
-
-
+​
 // From Multer documentation: https://github.com/expressjs/multer#diskstorage
-
-  
-  // The upload.single() middleware is used when a single file is expected:
-  // https://github.com/expressjs/multer#singlefieldname
-//   router.post('/', withAuth, upload.single('image'), async (req, res) => {
-//     // req.file is the 'image' file.
-  
-//     try {
-//       const newProject = await Character.create({
-//         ...req.body,
-//         // If no image was uploaded, undefined is returned, and the project is created with the default image.
-//         image: req.file && req.file.originalname,
-//         user_id: req.session.user_id,
-//       });
-  
-//       res.status(200).json(newProject);
-//     } catch (err) {
-//       console.log(err);
-//       res.status(400).json(err);
-//     }
-//   });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + "../../../public/img/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+​
+// The upload.single() middleware is used when a single file is expected:
+// https://github.com/expressjs/multer#singlefieldname
+router.post("/", /* withAuth,*/ upload.single("image"), async (req, res) => {
+  // req.file is the 'image' file.
+  console.log("req.file", req.file);
+​
+  try {
+    const characters = await Character.create({
+      ...req.body,
+      // If no image was uploaded, undefined is returned, and the project is created with the default image.
+      image: req.file && req.file.originalname,
+      user_id: req.session.user_id,
+    });
+    console.log(characters);
+    res.status(200).json(characters);
+    res.render("character-selection", { characters });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
 module.exports = router;
--- public/js/characterCreation.js
+++ public/js/characterCreation.js
@@ -1,35 +1,44 @@
 //create character event handler
-
+​
 const createCharacterHandler = async (event) => {
-    event.preventDefault();
-
-    const name = document.querySelector('.character-name').value.trim();
-    const raceResponse = document.querySelector('.character-race');
-    const race = raceResponse.options[raceResponse.selectedIndex].text;
-    const charClassResponse = document.querySelector('.character-class');
-    const char_class = charClassResponse.options[charClassResponse.selectedIndex].text;
-    const description = document.querySelector('.character-description').value.trim();
-    // const charPhoto = document.querySelector('#character-photo');
-    const image = document.getElementById('project-image').files[0];
-
-    if (name && race && char_class && description) {
-        const response = await fetch('/api/character', {
-            method: 'POST',
-            body: JSON.stringify({ name, race, char_class, description, image }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            // If successful, redirect the browser to the profile page
-            alert('The gods have smiled upon your creation. Go forth, mortal.');
-           
-            // document.location.replace('/character-selection');
-        } else {
-            alert('DISGRACE! You have angered the gods with your creation. Try again, mortal.');
-        }
+  event.preventDefault();
+​
+  const name = document.querySelector(".character-name").value.trim();
+  const raceResponse = document.querySelector(".character-race");
+  const race = raceResponse.options[raceResponse.selectedIndex].text;
+  const charClassResponse = document.querySelector(".character-class");
+  const char_class =
+    charClassResponse.options[charClassResponse.selectedIndex].text;
+  const description = document
+    .querySelector(".character-description")
+    .value.trim();
+  // const charPhoto = document.querySelector('#character-photo');
+  const image = document.getElementById("project-image").files[0];
+​
+  if (name && race && char_class && description) {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("race", race);
+    formData.append("char_class", char_class);
+    formData.append("description", description);
+    formData.append("image", image);
+​
+    const response = await fetch(`/api/character`, {
+      method: "POST",
+      body: formData,
+    });
+​
+    if (response.ok) {
+      console.log(response);
+      alert("The gods have smiled upon your creation. Go forth, mortal.");
+    } else {
+      alert(
+        "DISGRACE! You have angered the gods with your creation. Try again, mortal."
+      );
     }
+  }
 };
-
+​
 document
-    .querySelector('.character-creation-form')
-    .addEventListener('submit', createCharacterHandler);
+  .querySelector(".character-creation-form")
+  .addEventListener("submit", createCharacterHandler);
